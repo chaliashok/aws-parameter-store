1246fcb5407b7a1150f20e68b42107d98a15a4fb
chaliashok <chali.ashok@gmail.com>
2023-06-12 17:28:39 -0500

    aws parameter setup learning

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    { name= "roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.devopsawschinni.online:8080",type= "String" },
-    { name= "roboshop.dev.frontend.user_url", value="http://user-dev.devopsawschinni.online:8080",type= "String" },
-    { name= "roboshop.dev.frontend.cart_url", value="http://cart-dev.devopsawschinni.online:8080",type= "String" },
-    { name= "roboshop.dev.frontend.payment_url", value="http://payment-dev.devopsawschinni.online:8080",type= "String" },
-    { name= "roboshop.dev.frontend.shipping_url", value="http://shipping-dev.devopsawschinni.online:8080",type= "String" }
+    { name= "roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.devopsawschinni.online:8080/",type= "String" },
+    { name= "roboshop.dev.frontend.user_url", value="http://user-dev.devopsawschinni.online:8080/",type= "String" },
+    { name= "roboshop.dev.frontend.cart_url", value="http://cart-dev.devopsawschinni.online:8080/",type= "String" },
+    { name= "roboshop.dev.frontend.payment_url", value="http://payment-dev.devopsawschinni.online:8080/",type= "String" },
+    { name= "roboshop.dev.frontend.shipping_url", value="http://shipping-dev.devopsawschinni.online:8080/",type= "String" }
   ]
 }
